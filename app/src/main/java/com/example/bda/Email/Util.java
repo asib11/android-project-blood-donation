801c7f5219ecc0db1e5b4a8793e6abbadb2153e3
asib <ahmedasib1@gmail.com>
2023-01-13 22:04:39 +0600

    academic android base project named blood donation

---
+++ app/src/main/java/com/example/bda/Email/Util.java
@@ -0,0 +1,8 @@
+package com.example.bda.Email;
+
+public class Util {
+    // Add your own email and password over here.
+    public  static  final String EMAIL = "bappi.cse.bubt@gmail.com";
+    public static final String PASSWORD = "good boy";
+}
+
